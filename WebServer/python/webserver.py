6e8cf7f4d6dc3a105a1f36c2a9eada4ba576aed5
trinhgiahuy <65078173+trinhgiahuy@users.noreply.github.com>
2022-04-10 12:30:10 +0300

    Update webserver.py

--- WebServer/python/webserver.py
+++ WebServer/python/webserver.py
@@ -2,7 +2,17 @@
 
 ## HTTP server code
 
+
+
 import http.server
 import socketserver
 
 PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+#SimpleHTTPRequestHandler will look for a file named index.html in the current directory.
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:      #(ip address, port numner) ## TCP address includes IP addr & Port number
+  print("Serving at port ", PORT)
+  httpd.serve_forever()
+  
