9c68ae7a604c6a43d2ae94922328157314697903
trinhgiahuy <65078173+trinhgiahuy@users.noreply.github.com>
2022-04-11 18:00:50 +0300

    Update JerseyEmbeddedHTTPServer.java

--- WebServer/java/JerseyEmbeddedHTTPServer.java
+++ WebServer/java/JerseyEmbeddedHTTPServer.java
@@ -1,6 +1,16 @@
-//a very basic Embedded HTTP Jersey server (supporting only GET/POST) in Java using just the Java SE API,
-//without writing code to manually parse HTTP requests and manually format HTTP responses. 
-package my.Jersey.Java.tutorial
+// a very basic Embedded HTTP Jersey server (supporting only GET/POST) in Java using just the Java SE API,
+// without writing code to manually parse HTTP requests and manually format HTTP responses. 
+// => com.sun.net.httpserver.HttpServer, we should be able to achieve all above.
+
+//This file under package my.Jersey.Server.Java.tutorial
+package my.Jersey.Server.Java.tutorial
+
+/*TODO: 
+- Add `Cross-origin resource sharing` to Web Jersey Application
+- Convert project to Maven Project to simplify adding dependencies
+- Add `jersey-server` dependency
+- 
+*/
 
 import java.io.IOException;
 import java.net.InetAddress;
